b528e6080bf5f1cb4727b9225717ffb96996885f
EgyAbdo10 <egyabdo490@gmail.com>
2023-12-21 02:36:41 +0200

    added the destroy and all commands to the console

--- console.py
+++ console.py
@@ -6,6 +6,7 @@
 
 import cmd
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from models import storage
 
 
@@ -50,6 +51,37 @@ class HBNBCommand(cmd.Cmd):
         else:
             obj = storage.all()[f"{arg_list[0]}.{arg_list[1]}"]
             print(obj)
+
+    def do_destroy(self, line):
+        arg_list = line.split(" ")
+        if line == "":
+            print("** class name missing **")
+        elif arg_list[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+        elif len(arg_list) < 2:
+            print("** instance id missing **")
+        elif (
+            f"{arg_list[0]}.{arg_list[1]}" not in storage.all().keys()
+        ):
+            print("** no instance found **")
+        else:
+            storage._FileStorage__objects.__delitem__(
+                f"{arg_list[0]}.{arg_list[1]}"
+            )
+            storage.save()
+
+    def do_all(self, line):
+        arg_list = line.split(" ")
+        if line == "":
+            obj_list = (
+                [str(val) for val in storage.all().values()]
+            )
+        else:
+            obj_list = (
+                [str(val) for val in storage.all().values()
+                 if val.__class__.__name__ in arg_list]
+            )
+        print(obj_list)
 
 
 if __name__ == "__main__":
