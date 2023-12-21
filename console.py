ebe984ebc68495ed9a020d3ff64fd0f50d85f5ba
EgyAbdo10 <egyabdo490@gmail.com>
2023-12-21 02:00:33 +0200

    added teh create and show commands to the console

--- console.py
+++ console.py
@@ -5,6 +5,8 @@
 
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,6 +24,32 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, arg):
+        if arg in ["BaseModel"]:
+            obj = eval(arg)()
+            obj.save()
+            print(obj.id)
+        elif arg:
+            print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_show(self, line):
+        arg_list = line.split(" ")
+        if line == "":
+            print("** class name missing **")
+        elif arg_list[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+        elif len(arg_list) < 2:
+            print("** instance id missing **")
+        elif (
+            f"{arg_list[0]}.{arg_list[1]}" not in storage.all().keys()
+        ):
+            print("** no instance found **")
+        else:
+            obj = storage.all()[f"{arg_list[0]}.{arg_list[1]}"]
+            print(obj)
 
 
 if __name__ == "__main__":
